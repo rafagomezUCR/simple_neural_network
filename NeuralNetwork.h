a7d8d321dc65b657f3c5741d9ca0f549322a22f4
rafagomezUCR <rgome020@ucr.edu>
2021-07-08 20:29:08 -0700

    working network

--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -33,7 +33,7 @@ class NeuralNetwork {
                 Matrix weightMatrix(shape[i + 1], shape[i]);
                 initializeRandom(weightMatrix);
                 weights.push_back(weightMatrix);
-            }
+            } 
             for(int i = 0; i < shape.size() - 1; ++i){
                 Matrix biasMatrix(shape[i + 1], 1);
                 initializeRandom(biasMatrix);
@@ -73,95 +73,21 @@ class NeuralNetwork {
             Matrix error(targetOutput.size(), 1);
             error.matrix = targetOutput;
             Matrix output = values.back().negative();
-            for(int i = 0; i < output.rows; ++i){
-                for(int j = 0; j < output.cols; ++j){
-                    cout << output.at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            for(int i = 0; i < error.rows; ++i){
-                for(int j = 0; j < error.cols; ++j){
-                    cout << error.at(i, j) << " ";
-                }
-            }
-            cout << endl;
             error = error.matAdd(output);
-            error = error.square();
-            for(int i = 0; i < error.rows; ++i){
-                for(int j = 0; j < error.cols; ++j){
-                    cout << error.at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            Matrix dOutput = values[2].applyFunction(DSigmoid);
-            Matrix gradient = error.elementMult(dOutput);
-            gradient = gradient.scalarMult(learningRate);
-            for(int i = 0; i < gradient.rows; ++i){
-                for(int j = 0; j < gradient.cols; ++j){
-                    cout << gradient.at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            for(int i = 0; i < values[1].rows; ++i){
-                for(int j = 0; j < values[1].cols; ++j){
-                    cout << values[1].at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            Matrix tran2 = values[1].transpose();
-            cout << endl;
-            for(int i = 0; i < tran2.rows; ++i){
-                for(int j = 0; j < tran2.cols; ++j){
-                    cout << tran2.at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            gradient.size();
-            tran2.size();
-            Matrix weightGradients = tran2.matMult(gradient);
-            for(int i = 0; i < weightGradients.rows; ++i){
-                for(int j = 0; j < weightGradients.cols; ++j){
-                    cout << weightGradients.at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            for(int i = 0; i < weights[1].rows; ++i){
-                for(int j = 0; j < weights[1].cols; ++j){
-                    cout << weights[1].at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            weights[1] = weights[1].matAdd(weightGradients);
-            for(int i = 0; i < weights[1].rows; ++i){
-                for(int j = 0; j < weights[1].cols; ++j){
-                    cout << weights[1].at(i, j) << " ";
-                }
-            }
-            cout << endl;
-            bias[1] = bias[1].matAdd(gradient);
-
-            //Matrix trans = weights[1].transpose();
-            //Matrix prevError = error.matMult(trans);
-            Matrix dOutput2 = values[0 + 1].applyFunction(DSigmoid);
-            Matrix gradient2 = error.elementMult(dOutput2);
-            gradient2 = gradient2.scalarMult(learningRate);
-            Matrix tran22 = values[0].transpose();
-            Matrix weightGradients2 = tran22.matMult(gradient2);
-            bias[0] = bias[0].matAdd(gradient2);
-            /*for(int i = weights.size()-1; i >= 0; --i){
+            //error = error.square();
+            for(int i = weights.size()-1; i >= 0; --i){
                 Matrix trans = weights[i].transpose();
-                Matrix prevError = error.matMult(trans);
+                Matrix prevError = trans.matMult(error);
 
                 Matrix dOutput = values[i + 1].applyFunction(DSigmoid);
                 Matrix gradient = error.elementMult(dOutput);
                 gradient = gradient.scalarMult(learningRate);
                 Matrix tran2 = values[i].transpose();
-                Matrix weightGradients = tran2.matMult(gradient);
-
+                Matrix weightGradients = gradient.matMult(tran2);
                 weights[i] = weights[i].matAdd(weightGradients);
                 bias[i] = bias[i].matAdd(gradient);
                 error = prevError;
-            }*/
+            }
 
             return true;
         };
--- main.cpp
+++ main.cpp
@@ -1,37 +1,12 @@
 #include <iostream>
+#include <time.h>
 #include "NeuralNetwork.h"
 #include "Matrix.h"
 
 using namespace std;
 
 int main(){
-    /*Matrix m(2, 4);
-    Matrix m2(2, 3);
-    m.matrix[0] = 1;
-    m.matrix[1] = 2;
-    m.matrix[2] = 3;
-    m.matrix[3] = 1;
-    m.matrix[4] = 6;
-    m.matrix[5] = 8;
-    m.matrix[6] = 4;
-    m.matrix[7] = 3;
-
-    m2.matrix[0] = 2;
-    m2.matrix[1] = 1;
-    m2.matrix[2] = 1;
-    m2.matrix[3] = 6;
-    m2.matrix[4] = 3;
-    m2.matrix[5] = 4;
-
-    Matrix trans = m.transpose();
-    trans.print();
-    trans.size();
-
-    Matrix trans2 = m2.transpose();
-    trans2.print();
-    trans2.size();*/
-
-    
+    srand(time(0));
     vector<int> shape = {2, 3, 1};
     NeuralNetwork nn(shape, 0.1);
     vector<vector<float>> targetInputs = {
@@ -42,24 +17,22 @@ int main(){
     };
     vector<vector<float>> targetOutputs = {
         {0.0},
+        {1.0},
         {0.0},
-        {1.0},
-        {1.0},
+        {0.0},
     };
-    nn.feedForward(targetInputs[0]);
-    //nn.backPropagate(targetOutputs[0]);
-    int epoch = 1000;
+    int epoch = 100000;
     cout << "training" << endl;
     for(int i = 0; i < epoch; ++i){
         int index = rand() % 4;
         nn.feedForward(targetInputs[index]);
-        //nn.backPropagate(targetOutputs[index]);
+        nn.backPropagate(targetOutputs[index]);
     }
     cout << "training done" << endl;
     for(int i = 0; i < targetInputs.size(); ++i){
         nn.feedForward(targetInputs[i]);
         vector<float> preds = nn.getPred();
-        cout << targetInputs[i][0] << "," << targetInputs[i][1] << "->" << preds[i] << endl;
+        cout << targetInputs[i][0] << "," << targetInputs[i][1] << "->" << preds[0] << endl;
     }
     return 0;
 }
